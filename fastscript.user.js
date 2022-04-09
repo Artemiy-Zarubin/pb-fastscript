7b7842b3e1c04f032f65ebfb5334391e82071220
Artemiy Zarubin <101475420+ArtemiyGeneralov@users.noreply.github.com>
2022-04-09 17:31:12 +0300

    Update fastscript.user.js

--- fastscript.user.js
+++ fastscript.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         PB KittyScript
+// @name         PB FastScript
 // @version      1.0
 // @description  Script-assistant for Pixel Battle.
 // @author       Artemiy Zarubin
