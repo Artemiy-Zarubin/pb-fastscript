afc392cbdc704508c13539da2a93db4e8816b047
Artemiy Zarubin <101475420+Artemiy-Zarubin@users.noreply.github.com>
2022-04-10 13:01:40 +0300

    Update fastscript.user.js

--- fastscript.user.js
+++ fastscript.user.js
@@ -58,16 +58,36 @@ setTimeout(() => { setInterval(showCaptchaBeforeCollect, 500) },1000);
 function showCountPaints() {
     let element = document.getElementsByClassName('paintPerHour')[0],
         countPaints = document.getElementsByTagName('p')[15];
-    if(countPaints && Number(countPaints.textContent) > 1000) {
-        countPaints.textContent = '1000';
+    let diff = Number(countPaints.textContent) - 1000;
+    if(Number(countPaints.textContent) > 1000) {
+        minusColors(diff)
     }
-    if(element && element.style.display !== 'block') {
-        element.style.display = 'block';
-    }
+    
 }
 setInterval(showCountPaints, 800);
 
+function pixinfoDesign() {
+    let modalinfo = document.getElementsByClassName('pixel_info')[0],
+        imodal = modalinfo.getElementsByClassName('main')[0],
+        uname = imodal.getElementsByClassName('name')[0],
+        cname = imodal.getElementsByClassName('clan')[0],
+        icoord = imodal.getElementsByClassName('coord')[0],
+        idate = imodal.getElementsByClassName('date')[0],
+        iavatar = imodal.getElementsByClassName('avatar')[0],
+        ibordertop = imodal.getElementsByClassName('arrow')[0],
+        mitext = modalinfo.firstElementChild;
+    if(!modalinfo || !imodal || !uname || !cname || !icoord || !idate || !iavatar || !ibordertop || !mitext) return;
+    mitext.style.color = '#fff';
+    ibordertop.style['border-top'] = '10px solid #5d5da6'; // #5858ff
+    iavatar.style['border-radius'] = '50%';
+    modalinfo.style['border-radius'] = '8px';
+    modalinfo.style['background-color'] = '#5d5da6'
+    uname.style.color = '#fff';
+    cname.style.color = '#fff';
+    icoord.style.color = '#c4c4ee';
+    idate.style.color = '#c4c4ee';
+};
 
-
+pixinfoDesign();
 changeDesign();
 fastCaptchaClick();
